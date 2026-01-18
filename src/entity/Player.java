05a8ac44dba74b4119e6d00c173fb0fbcbeffe6c
AndrewH2O <AndrewH2O@users.noreply.github.com>
2026-01-18 18:01:43 +0000

    Add player sprite and make it animate

---
+++ src/entity/Player.java
@@ -0,0 +1,96 @@
+package entity;
+
+import main.GamePanel;
+import main.KeyHandler;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+public class Player extends Entity {
+	GamePanel gamePanel;
+	KeyHandler keyHandler;
+
+	public Player(GamePanel gamePanel, KeyHandler keyHandler) {
+		this.gamePanel = gamePanel;
+		this.keyHandler = keyHandler;
+		setDefaults();
+		getPlayerImage();
+	}
+
+	public void setDefaults() {
+		x = 100;
+		y = 100;
+		speed = 4;
+		direction = "down";
+	}
+
+	public void update() {
+		if(keyHandler.upPressed) {
+			direction = "up";
+			y -= speed;
+		}
+		else if(keyHandler.downPressed) {
+			direction = "down";
+			y += speed;
+		}
+		else if(keyHandler.leftPressed) {
+			direction = "left";
+			x -= speed;
+		}
+		else if(keyHandler.rightPressed) {
+			direction = "right";
+			x += speed;
+		}
+
+		// update animation as this runs 60 times per second
+		// every 10 count the spriteNum is changed between 1 and 2
+		spriteCounter++;
+		if(spriteCounter >= 12) {
+			if (spriteNum == 1) spriteNum = 2;
+			else if (spriteNum == 2) spriteNum = 1;
+			spriteCounter = 0;
+		}
+	}
+
+	public void draw(Graphics2D g) {
+		//g.setColor(Color.WHITE);
+		//g.fillRect(x, y, gamePanel.tileSize, gamePanel.tileSize);
+		BufferedImage image = null;
+		switch (direction) {
+			case "up":
+				if(spriteNum == 1)image = up1;
+				if(spriteNum == 2)image = up2;
+				break;
+			case "down":
+				if(spriteNum == 1)image = down1;
+				if(spriteNum == 2)image = down2;
+				break;
+			case "left":
+				if(spriteNum == 1)image = left1;
+				if(spriteNum == 2)image = left2;
+				break;
+			case "right":
+				if(spriteNum == 1)image = right1;
+				if(spriteNum == 2)image = right2;
+				break;
+		}
+		g.drawImage(image, x, y, gamePanel.tileSize, gamePanel.tileSize, null);
+	}
+
+	public void getPlayerImage() {
+		try {
+			up1 = ImageIO.read(getClass().getResourceAsStream("/player/boy_up_1.png"));
+			up2 = ImageIO.read(getClass().getResourceAsStream("/player/boy_up_2.png"));
+			down1 = ImageIO.read(getClass().getResourceAsStream("/player/boy_down_1.png"));
+			down2 = ImageIO.read(getClass().getResourceAsStream("/player/boy_down_2.png"));
+			left1 = ImageIO.read(getClass().getResourceAsStream("/player/boy_left_1.png"));
+			left2 = ImageIO.read(getClass().getResourceAsStream("/player/boy_left_2.png"));
+			right1 = ImageIO.read(getClass().getResourceAsStream("/player/boy_right_1.png"));
+			right2 = ImageIO.read(getClass().getResourceAsStream("/player/boy_right_2.png"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -1,3 +1,7 @@
+package main;
+
+import entity.Player;
+
 import javax.swing.*;
 import java.awt.*;
 
@@ -10,20 +14,18 @@ public class GamePanel extends JPanel implements Runnable{
 	// old games had small screens e.g. 320 by 224 so we scale
 	final int scale = 3; // scaling factor larger screens
 
-	final int tileSize = originalTileSize * scale; // 48 * 48
+	public  int tileSize = originalTileSize * scale; // 48 * 48
 	final int maxScreenCol = 16;
 	final int maxScreenRow = 12;
 	final int screenWidth = tileSize * maxScreenCol; // 768
 	final int screenHeight = tileSize * maxScreenRow; // 576
 
 	KeyHandler keyHandler = new KeyHandler();
-	// players default position
-	int playerX = 100;
-	int playerY = 100;
-	int playerSpeed = 4;
+
 
 	//Time
 	Thread gameThread;
+	Player player = new Player(this, keyHandler);
 
 	public GamePanel() {
 		setPreferredSize(new Dimension(screenWidth, screenHeight));
@@ -104,26 +106,15 @@ public class GamePanel extends JPanel implements Runnable{
 	}
 
 	public void update() {
-		if(keyHandler.upPressed) {
-			playerY -= playerSpeed;
-		}
-		else if(keyHandler.downPressed) {
-			playerY += playerSpeed;
-		}
-		else if(keyHandler.leftPressed) {
-			playerX -= playerSpeed;
-		}
-		else if(keyHandler.rightPressed) {
-			playerX += playerSpeed;
-		}
+		player.update();
 	}
 
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		Graphics2D g2 = (Graphics2D) g;// gives us better geometry
-		g2.setColor(Color.WHITE);
 
-		g2.fillRect(playerX, playerY, tileSize, tileSize);
+		player.draw(g2);
+
 		g2.dispose();
 	}
 }
