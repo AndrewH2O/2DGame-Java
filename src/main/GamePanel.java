42e8455c4acf188097195d5a6665ee5dafcfc342
AndrewH2O <AndrewH2O@users.noreply.github.com>
2026-01-27 23:06:23 +0000

    Fix bug where sound effect playing interrupts the looping music. Now in separate Sound classes

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -30,7 +30,8 @@ public class GamePanel extends JPanel implements Runnable{
 	// SYSTEM SETTINGS
 	KeyHandler keyHandler = new KeyHandler();
 	TileManager tileManager = new TileManager(this);
-	Sound sound = new Sound();
+	Sound music = new Sound();
+	Sound soundEffect = new Sound();
 
 	//collision detection
 	public CollisionChecker collisionChecker = new CollisionChecker(this);
@@ -146,17 +147,17 @@ public class GamePanel extends JPanel implements Runnable{
 	}
 
 	public void playMusic(int i){
-		sound.setFile(i);
-		sound.play();
-		sound.loop();
+		music.setFile(i);
+		music.play();
+		music.loop();
 	}
 
 	public void stopMusic(){
-		sound.stop();
+		music.stop();
 	}
 
 	public void playSoundEffect(int i){
-		sound.setFile(i);
-		sound.play();
+		soundEffect.setFile(i);
+		soundEffect.play();
 	}
 }
